08f1181bdebc89d884d8cf27071cf201c08fe4bb
DevMahlatsi <j.mahlatsi@outlook.com>
2025-12-08 20:04:21 +0200

    Minor fixes and imporovements

--- src/Layout/MovieLayout.tsx
+++ src/Layout/MovieLayout.tsx
@@ -3,7 +3,7 @@ import type { MovieLayoutProps } from "../types/props";
 export default function MovieLayout({children}: MovieLayoutProps){
   return(
     <>
-    <div className='text-white-800 flex flex-wrap gap-2   justify-center'>
+    <div className='text-white-800 flex flex-wrap gap-2  justify-center'>
       {children}
     </div>
     </>

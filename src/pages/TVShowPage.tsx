1582fb3c80226556ad157c850a519052dfad8d28
DevMahlatsi <j.mahlatsi@outlook.com>
2025-12-13 18:58:02 +0200

    Pirates Won. We remain humble. Today working on the TV Show page and Movie page so the navigation panel is actuall useful.

---
+++ src/pages/TVShowPage.tsx
@@ -0,0 +1,12 @@
+export default function(){
+  return(
+    <>
+      This is the TVShow page when a user wishes to see all the TV SHows. I think I plan on allowing them to view the trending, popular and some random stuff.
+      Ideas are 
+      the thing that automatically goes from left to right instead of sliding down
+      ...edit, it's a carousel.
+      Yeah same thing for movies too, I think it's there we can actually practice the whole thing of filtering too at api level or it would mean more work on the frontend for me and that's a no.
+      
+    </>
+  )
+}

b0a468aca3dbcd96fee5309fec6beba970ab1ab7
DevMahlatsi <j.mahlatsi@outlook.com>
2025-12-14 20:12:09 +0200

    MoviePage now supports all categories of the moveies.
    That is {moviesPlaying, Upcoming movies, Top rated movies, popular movies}
    
    Next is TV shows.

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -3,25 +3,54 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NavBar from "../Layout/Navbar";
 import type { MediaItem } from "../types/movies";
-import { getNowPlaying } from "../api/movie.api";
+import { getNowPlaying, getPopularMovies, getTopRatedMovies, getUpcomingMovies } from "../api/movie.api";
 import MovieLayout from "../Layout/MovieLayout";
 import { MovieCard } from "../components/MovieCard";
 
 export default function(){
   const navigate = useNavigate();
   const [nowPlaying, setNowPlaying] = useState<MediaItem[]>([]);
+  const [topRated, setTopRated] = useState<MediaItem[]>([]);
+  const [upcomingMovies, setUpcomingMovies] = useState<MediaItem[]>([]);
+  const [popularMovies, setPopularMovies] = useState<MediaItem[]>([]);
   const [moviesLoading, setMoviesLoading] = useState<boolean>(true);
   
   useEffect(() => {
     async function fetchAll(){
       try {
-              const movies = await getNowPlaying();
-              setNowPlaying(movies);
-            } catch (err) {
-              console.error('Fetch error for movies:', err);
-            } finally {
-              setMoviesLoading(false);
-            }
+        const movies = await getNowPlaying();
+        setNowPlaying(movies);
+      } catch (err) {
+        console.error('Fetch error for movies:', err);
+      } finally {
+        setMoviesLoading(false);
+      }
+      try{
+        const movies = await getTopRatedMovies();
+        setTopRated(movies);
+      }catch(err){
+        console.log("Error fetching top rated movies", err);
+      }
+      finally{
+        setMoviesLoading(false);
+      }
+      try{
+        const movies = await getPopularMovies();
+        setPopularMovies(movies);
+      }catch(err){
+        console.log("Error fetching top rated movies", err);
+      }
+      finally{
+        setMoviesLoading(false);
+      }
+      try{
+        const movies = await getUpcomingMovies();
+        setUpcomingMovies(movies);
+      }
+      catch(err){
+        console.error("Error while fetching upcoming movies.");
+      }
+   
     }
     fetchAll();     
   },
@@ -48,6 +77,66 @@ export default function(){
         ) :(
           <MovieLayout>
             {nowPlaying.map((movie) =>(
+              <MovieCard
+              key={movie.id}
+              movie = {movie}
+              onClick={() => handleMovieClick(movie)}
+            />
+            ))}
+          </MovieLayout>
+        )}
+      </section>
+      <p className="text-purple-500 text-left text-3xl p-2">
+          Top Rated Movies
+        </p>
+        <section className="for-the-movies">
+        {moviesLoading? (
+          <p>Loading Movies...</p>
+        ): topRated.length === 0 ? (
+          <p> No movies available</p>
+        ) :(
+          <MovieLayout>
+            {topRated.map((movie) =>(
+              <MovieCard
+              key={movie.id}
+              movie = {movie}
+              onClick={() => handleMovieClick(movie)}
+            />
+            ))}
+          </MovieLayout>
+        )}
+      </section>  
+      <p className="text-purple-500 text-left text-3xl p-2">
+          Popular Movies
+        </p>
+        <section className="for-the-movies">
+        {moviesLoading? (
+          <p>Loading Movies...</p>
+        ): popularMovies.length === 0 ? (
+          <p> No movies available</p>
+        ) :(
+          <MovieLayout>
+            {popularMovies.map((movie) =>(
+              <MovieCard
+              key={movie.id}
+              movie = {movie}
+              onClick={() => handleMovieClick(movie)}
+            />
+            ))}
+          </MovieLayout>
+        )}
+      </section>  
+      <p className="text-purple-500 text-left text-3xl p-2">
+          Upcoming Movies
+        </p>
+        <section className="for-the-movies">
+        {moviesLoading? (
+          <p>Loading Movies...</p>
+        ): upcomingMovies.length === 0 ? (
+          <p> No movies available</p>
+        ) :(
+          <MovieLayout>
+            {upcomingMovies.map((movie) =>(
               <MovieCard
               key={movie.id}
               movie = {movie}
